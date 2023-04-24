1b87b10bf89198cca1b7d42f522e51a331bab338
Akshay Kumar <akshay.as597@gmail.com>
2023-04-24 22:12:03 +0530

    TextField_Length
    -> TextField_Length Extension

---
+++ Extentions/Extensions/UITextFied+Extension.swift
@@ -0,0 +1,41 @@
+//
+//  UITextFied+Extension.swift
+//  Extentions
+//
+//  Created by Akshay Kumar on 24/04/23.
+//
+
+import Foundation
+import UIKit
+
+private var KeyMaxLength: Int = 0
+
+extension UITextField {
+    
+    @IBInspectable var maxLength: Int {
+        get {
+            if let length = objc_getAssociatedObject(self, &KeyMaxLength) as? Int {
+                return length
+            } else {
+                return Int.max
+            }
+        }
+        set {
+            objc_setAssociatedObject(self, &KeyMaxLength, newValue, .OBJC_ASSOCIATION_RETAIN)
+            addTarget(self, action: #selector(checkMaxLength), for: .editingChanged)
+        }
+    }
+    
+    @objc func checkMaxLength(textField: UITextField) {
+        guard let prospectiveText = self.text,
+            prospectiveText.count > maxLength
+            else {
+                return
+        }
+        
+        let selection = selectedTextRange
+        let maxCharIndex = prospectiveText.index(prospectiveText.startIndex, offsetBy: maxLength)
+        text = prospectiveText.substring(to: maxCharIndex)
+        selectedTextRange = selection
+    }
+}
