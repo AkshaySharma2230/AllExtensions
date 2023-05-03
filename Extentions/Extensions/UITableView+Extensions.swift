b08c3278b45052e70e82b2f594970a9731b21523
AkshaySharma <akshay.as597@gmail.com>
2023-05-03 22:21:06 +0530

    UITableViewExtensions
    -> TableView Reload With Animation

---
+++ Extentions/Extensions/UITableView+Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  UITableView+Extensions.swift
+//  Extentions
+//
+//  Created by Akshay Kumar on 03/05/23.
+//
+
+import Foundation
+import AVKit
+import AVFoundation
+
+enum AnimationDirection: String {
+    case up
+    case down
+    case left
+    case right
+    case none
+}
+
+extension UITableView {
+     func reloadWithAnimation(animationDirection:AnimationDirection) {
+         self.reloadData()
+         guard animationDirection != .none else { return }
+         self.layoutIfNeeded()
+        let cells = self.visibleCells
+        var index = 0
+        let tableHeight: CGFloat = self.bounds.size.height
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            switch animationDirection {
+            case .up:
+                cell.transform = CGAffineTransform(translationX: 0, y: -tableHeight)
+                break
+            case .down:
+                cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+                break
+            case .left:
+                cell.transform = CGAffineTransform(translationX: tableHeight, y: 0)
+                break
+            case .right:
+                cell.transform = CGAffineTransform(translationX: -tableHeight, y: 0)
+                break
+            default:
+                cell.transform = CGAffineTransform(translationX: tableHeight, y: 0)
+                break
+            }
+            UIView.animate(withDuration: 1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0);
+            }, completion: nil)
+            index += 1
+        }
+    }
+}
