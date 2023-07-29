72e08c5bad84265c842a4b14daf71cbfa1f1f15e
AkshaySharma <akshay.as597@gmail.com>
2023-07-29 23:15:20 +0530

    Image rendering Function

--- Extentions/Extensions/UIVIew+Extension.swift
+++ Extentions/Extensions/UIVIew+Extension.swift
@@ -19,4 +19,20 @@ extension UIView {
         NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
         NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
     }
+    
+    
+    func asImage(size: CGRect) -> UIImage {
+           if #available(iOS 10.0, *) {
+               let renderer = UIGraphicsImageRenderer(bounds: size)
+               return renderer.image { rendererContext in
+                   layer.render(in: rendererContext.cgContext)
+               }
+           } else {
+               UIGraphicsBeginImageContext(self.bounds.size)
+               self.layer.render(in: UIGraphicsGetCurrentContext()!)
+               let image = UIGraphicsGetImageFromCurrentImageContext()
+               UIGraphicsEndImageContext()
+               return UIImage(cgImage: image!.cgImage!)
+           }
+       }
 }
