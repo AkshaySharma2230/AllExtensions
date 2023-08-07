d5bbee9237ab366791d3e95c581538ff191ec159
AkshaySharma <akshay.as597@gmail.com>
2023-08-07 23:32:33 +0530

    New Changes

--- Extentions/Extensions/UIVIew+Extension.swift
+++ Extentions/Extensions/UIVIew+Extension.swift
@@ -22,17 +22,45 @@ extension UIView {
     
     
     func asImage(size: CGRect) -> UIImage {
-           if #available(iOS 10.0, *) {
-               let renderer = UIGraphicsImageRenderer(bounds: size)
-               return renderer.image { rendererContext in
-                   layer.render(in: rendererContext.cgContext)
-               }
-           } else {
-               UIGraphicsBeginImageContext(self.bounds.size)
-               self.layer.render(in: UIGraphicsGetCurrentContext()!)
-               let image = UIGraphicsGetImageFromCurrentImageContext()
-               UIGraphicsEndImageContext()
-               return UIImage(cgImage: image!.cgImage!)
-           }
-       }
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: size)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.bounds.size)
+            self.layer.render(in: UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
+    }
+    
+    /** Loads instance from nib with the same name. */
+    func loadNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nibName = type(of: self).description().components(separatedBy: ".").last!
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as! UIView
+    }
+    
+    func addBluerToView() {
+        var bluerView: UIBlurEffect!
+        bluerView = UIBlurEffect(style: .regular)
+        let bluerEffextView = UIVisualEffectView(effect: bluerView)
+        bluerEffextView.alpha = 0.5
+        bluerEffextView.backgroundColor = .black
+        bluerEffextView.backgroundColor?.withAlphaComponent(0.5)
+        self.addSubview(bluerEffextView)
+        bluerEffextView.frame = self.frame
+        
+    }
+    
+    func removeBluerFromView() {
+        self.subviews.forEach { (view) in
+            if view is UIVisualEffectView {
+                view.removeFromSuperview()
+            }
+        }
+    }
 }
